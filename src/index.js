746024f4112ae91186fdb79c609e1332038d7694
distanceOn <distanceOn@yandex.ru>
2023-02-12 19:51:26 +0300

    Create logic for rendering a list of elements from the API and then going to the page with the content rendering according to the GET parameters

--- src/index.js
+++ src/index.js
@@ -4,5 +4,20 @@ request({
   path: "data",
   onSuccess: (data) => {
     console.log(data);
+    for (const item of data) {
+      renderList(item);
+    }
   },
 });
+
+function renderList(item) {
+  const list = document.querySelector(".main__list");
+  const li = document.createElement("li");
+  li.classList.add("list__item");
+  const a = document.createElement("a");
+  a.classList.add("list__a");
+  a.textContent = item;
+  a.setAttribute("href", `../single.html?s=${item.toLowerCase()}`);
+  li.append(a);
+  list.append(li);
+}
